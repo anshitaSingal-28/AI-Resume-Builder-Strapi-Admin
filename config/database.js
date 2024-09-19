dda869cfb649ed46f47a09490ee996daa6b117f2
Anshita <singalanshita@gmail.com>
2024-09-19 22:07:48 +0530

    Initial Commit

---
+++ config/database.js
@@ -0,0 +1,85 @@
+const path = require("path");
+
+module.exports = ({ env }) => {
+  const client = env("DATABASE_CLIENT", "postgres");
+
+  const connections = {
+    mysql: {
+      connection: {
+        host: env(
+          "DATABASE_HOST",
+          "ep-billowing-bread-a50v68rs.us-east-2.aws.neon.tech"
+        ),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          key: env("DATABASE_SSL_KEY", undefined),
+          cert: env("DATABASE_SSL_CERT", undefined),
+          ca: env("DATABASE_SSL_CA", undefined),
+          capath: env("DATABASE_SSL_CAPATH", undefined),
+          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
+        },
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+    },
+    postgres: {
+      connection: {
+        connectionString: env(
+          "DATABASE_URL",
+          "postgresql://Resume_Database_owner:fUPFp0TxV8Bm@ep-billowing-bread-a50v68rs.us-east-2.aws.neon.tech/Resume_Database?sslmode=require"
+        ),
+        // host: env(
+        //   "DATABASE_HOST",
+        //   "ep-billowing-bread-a50v68rs.us-east-2.aws.neon.tech"
+        // ),
+        // port: env.int("DATABASE_PORT", 5432),
+        // database: env("DATABASE_NAME", "Resume_Database"),
+        // user: env("DATABASE_USERNAME", "Resume_Database_owner"),
+        // password: env("DATABASE_PASSWORD", "fUPFp0TxV8Bm"),
+        ssl: env.bool("DATABASE_SSL", true) && {
+          key: env("DATABASE_SSL_KEY", undefined),
+          cert: env("DATABASE_SSL_CERT", undefined),
+          ca: env("DATABASE_SSL_CA", undefined),
+          capath: env("DATABASE_SSL_CAPATH", undefined),
+          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
+        },
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    },
+  };
+};
